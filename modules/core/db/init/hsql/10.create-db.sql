6393caf587d60b8f40bc8dccd243181b2b43daff
Konstantin Krivopustov <krivopustov@haulmont.com>
2017-02-22 14:10:42 +0400

    Add an entity to temporarily overcome the issue in Studio with removing content of 30.create-db.sql when starting app server and updating database in background

--- modules/core/db/init/hsql/10.create-db.sql
+++ modules/core/db/init/hsql/10.create-db.sql
@@ -1,1 +1,16 @@
-
+-- begin SAMPLE_SOME_ENTITY
+create table SAMPLE_SOME_ENTITY (
+    ID varchar(36) not null,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NAME varchar(255),
+    --
+    primary key (ID)
+)^
+-- end SAMPLE_SOME_ENTITY
